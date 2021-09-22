ecf593946268fee20d174735a3731d0ece053fd0
mrs-matheus <mrs.matheusreis@gmail.com>
2021-09-22 19:27:47 -0300

    Auth+register+token

---
+++ API/RoleTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTarefa.API
+{
+    public enum RoleTypes
+    {
+        Common
+    }
+}
